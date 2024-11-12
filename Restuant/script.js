62f83055346826ff734c12e23b6c8885a8b8a2f6
azizboujaada <azizboujaada@gmail.com>
2024-11-12 19:57:08 +0100

    add some effects with javascript

--- Restuant/script.js
+++ Restuant/script.js
@@ -1,18 +1,47 @@
 
+let activeCard=1;
+const cards= document.querySelectorAll(".cards");
+
+
+function cardColor(){
+  cards.forEach((cards ,index) => {
+    if (index=== activeCard) {
+        cards.classList.add("center")
+    }else{
+        cards.classList.remove("center");
+    }
+  });
+}
+
+
+
+
 function scrollRight() {
     const container = document.getElementById("client_cards");
+    if(activeCard<cards.length-1){
+        activeCard++;
+    }
     container.scrollBy({
         left: 450,
         behavior: "smooth"
     });
+
+    cardColor();
 }
 function scrollToLeft(){
     const container =document.getElementById("client_cards");
+    if(activeCard>0){
+        activeCard--;
+    }
     container.scrollBy({
         left: -450,
         behavior:"smooth"
     }); 
+
+    cardColor();
 }
+
+cardColor();
 
 
 function scrollIntoBottom(){
