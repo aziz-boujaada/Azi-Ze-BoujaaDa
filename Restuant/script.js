e26636fc184ba3b9b860f9d60f5b205d957ad656
azizboujaada <azizboujaada@gmail.com>
2024-11-13 21:06:21 +0100

    fixed switch between page problems & add more effects with javaScript

--- Restuant/script.js
+++ Restuant/script.js
@@ -1,8 +1,77 @@
+/* FUNCTION TO VISIBLE AND HIDDEN A DROPDOWN LIST IN HEADER (PART PAGES) */
+const drop_down= document.querySelector(".drop_down");
+const toggleDrop = document.getElementById("toggleDrop")
+function hiddenDrop(){
+   
+    drop_down.classList.toggle("hidden");
+    if(drop_down.classList.contains("hidden")){
+        console.log("the drop is now hidden  ");
+    }else{
+        console.log("drop now is visible");
+    }
+}
+toggleDrop.addEventListener("click",hiddenDrop);
 
+/* FUNCTION TO SCROLL BOTTOM IN ABOUT PAGE =>SCROLLING FROM ABOUT US TO MASTER CHEFS */
+function scrollIntoBottom(){
+    console.log("function called");
+    const nextPage = document.getElementById("resto_members");
+    if(nextPage){
+        nextPage.scrollIntoView({
+            behavior:"smooth",
+            block:"start",
+        });
+    }
+    else{
+        console.log("function doesn't work");
+    }
+}
+
+
+
+/* FUNCTION TO SWITCH BETWEEN FOOD TIME ( BREAKFAST-LUNCH-DINNER)*/
+
+ 
+
+const food_time = document.querySelectorAll(".food_time  .time");
+function foodTimeSwitch(){
+ food_time.forEach((food)=>{
+   food.addEventListener("click", (nomove)=>{
+    nomove.preventDefault();
+    food_time.forEach((item) => item.classList.remove("active_time"));
+
+    food.classList.add("active_time");
+   });
+});
+
+  console.log("function called");
+}
+foodTimeSwitch();
+
+
+
+/* FUNCTION TO SWITCH INTO OTHER MENU TIME FOOD  */
+function switchMenu(){
+    console.log("function called");
+    const nextmenu = document.querySelectorAll(".time ");
+       
+           nextmenu.forEach((next) => {
+
+
+        next.scrollIntoView({
+            behavior:"smooth",
+            block:"start"
+        });
+    });
+   
+}
+switchMenu();
+
+
+  /*TWO FUNCTIONS TO SCROLL THE TESTIMONIAL TO LEFT & RIGHT AND CHANGE COLOR OF THE ACTIVE CARDS */
+  /*change testimonial active card color  */
 let activeCard=1;
 const cards= document.querySelectorAll(".cards");
-
-
 function cardColor(){
   cards.forEach((cards ,index) => {
     if (index=== activeCard) {
@@ -13,9 +82,7 @@ function cardColor(){
   });
 }
 
-
-
-
+/* scrolling to right */
 function scrollRight() {
     const container = document.getElementById("client_cards");
     if(activeCard<cards.length-1){
@@ -28,6 +95,7 @@ function scrollRight() {
 
     cardColor();
 }
+/* scrolling to left */
 function scrollToLeft(){
     const container =document.getElementById("client_cards");
     if(activeCard>0){
@@ -44,16 +112,3 @@ function scrollToLeft(){
 cardColor();
 
 
-function scrollIntoBottom(){
-    console.log("function called");
-    const nextPage = document.getElementById("resto_members");
-    if(nextPage){
-        nextPage.scrollIntoView({
-            behavior:"smooth",
-            block:"start",
-        });
-    }
-    else{
-        console.log("function doesn't work");
-    }
-}
